1ead32543c475c1702dc7429f41933605bb9a3b3
salih <muhammetsalihaslan@gmail.com>
2022-06-27 14:40:31 +0300

    up1

---
+++ first-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Component from './component/Component';
+
+const App = () => {
+  return (
+    <div>
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/component/Component.jsx
@@ -0,0 +1,22 @@
+import './Component.scss';
+
+const Component = () => {
+    return(
+        <div className="container">
+            <div className="container2">
+            <section className="screen">
+               
+            </section>
+            <div className="button">
+                <button className="tur">Tur</button>
+                <button className="start">Start</button>
+            </div>
+            <section className="tourTime">
+            
+            </section>
+        </div>
+        </div>
+    )
+}
+
+export default Component;
